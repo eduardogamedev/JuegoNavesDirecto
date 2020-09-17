338a8a3a1948c3ff0ca8ca9d82cdf9094b91af16
Eduardo Carpintero <eduardo.carpinter94@gmail.com>
2020-09-17 15:29:48 +0200

    Balanceo del avion

--- PlaneProject/Assets/Scripts/PlaneEnemy.cs
+++ PlaneProject/Assets/Scripts/PlaneEnemy.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class PlaneEnemy : Enemy
 {
-
-    public UnityStandardAssets.Utility.WaypointCircuit waypointCircuit;
-    public float speed = 100f;
-    public float speedRotation = 15f;
+    [SerializeField]
+    private UnityStandardAssets.Utility.WaypointCircuit waypointCircuit;
+    [SerializeField]
+    private float speed = 100f;
+    [SerializeField]
+    private float speedRotation = 15f;
+    [SerializeField]
+    private float rotationAngle = 15f;
 
     private Transform[] wayPoints;
-    private List<Transform> wayPointsList = new List<Transform>();
+    private int idWayPoints = 0;
     private Transform destination;
+
+    private Quaternion targetRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         wayPoints = waypointCircuit.Waypoints;
         GenerateDestination();
+
+        targetRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -25,25 +33,35 @@ public class PlaneEnemy : Enemy
     {
         transform.position = Vector3.MoveTowards(transform.position,destination.position,
         speed*Time.deltaTime);
+        float distance = Vector3.Distance(transform.position,destination.position);
 
         Quaternion rotation = Quaternion.LookRotation(destination.position - transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation,rotation,Time.deltaTime*speedRotation);
-        //Vector3 direction = (destination.position - transform.position).normalized;
-        
+        //float yRotationDirection = (Quaternion.Inverse(targetRotation) * transform.rotation).eulerAngles.y;
 
+        Vector3 targetDir = destination.position - transform.position;
+        Vector3 forward = transform.forward;
+        
+        float angle = Vector3.Angle(targetDir, forward);
+        rotation = rotation* Quaternion.Euler(Vector3.forward*angle);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation,rotation,Time.deltaTime*speedRotation);
+        
         //Debug.Log(Vector3.Distance(transform.position,destination.position));
-        if(Vector3.Distance(transform.position,destination.position)<=0.5f){
+        if(distance<=0.5f){
             GenerateDestination();
+        }
+ 
+        if(targetRotation != transform.rotation){
+            targetRotation = transform.rotation;
         }
     }
 
     private void GenerateDestination(){
-        if(wayPointsList.Count==0){
-            wayPointsList.AddRange(wayPoints);
+        destination = wayPoints[idWayPoints];
+        idWayPoints++;
+        if(idWayPoints >= wayPoints.Length){
+            idWayPoints = 0;
         }
-        int id = Random.Range(0,wayPointsList.Count);
-        destination = wayPointsList[id];
-        wayPointsList.RemoveAt(id);
     }
     
 }
